7b65a5d6e2a822931e26623949eaf87a8f917149
Krzysztofowi bojarczuk <krzysztofbojarczuk@wp.pl>
2021-10-12 14:20:21 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./logo.svg";
+import beachwork from "./beach-work.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+      <div className="bg-red-100">
+        
+        <div className="px-8 py-12">
+        <img className="h-10" src={logo} alt="Workcation" />
+        <img className="mt-6 rounded-lg shadow-xl" src={beachwork} alt="Woman workcationing on the beach" />
+        <h1 className="mt-6 text-2x1 font-bold text-gray-900">You can work from anywhere. <span className="text-indigo-500"> Take advantage of it. </span></h1>
+       <p className="mt-6 text-gray-600">Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some
+        nice weather even when you're not on vacation.</p>
+        <div className="mt-4">
+          <a className="inline-block rounded-lg px-5 py-3 shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm" href="">Book your escape</a>
+        </div>
+        </div>
+      </div>
+      
+    
+  </div>
   );
 }
 
