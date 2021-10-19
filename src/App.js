02fa55f8bb3d7bd15f1b4f97a335e90cb42690c5
Krzysztofowi bojarczuk <krzysztofbojarczuk@wp.pl>
2021-10-19 08:32:57 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import './App.css';
 import logo from "./logo.svg";
+import logobrand from "./logo-brand.svg"
 import beachwork from "./beach-work.jpg"
+import popularDestinations from './data/popularDestinations';
+import DestinationCard from "./components/DestinationCard";
+
 
 function App() {
+  
   return (
 
-    <div>
-      <div className="bg-red-100">
-        
-        <div className="px-8 py-12 max-w-md mx-auto">
-        <img className="h-10" src={logo} alt="Workcation" />
-        <img className="mt-6 rounded-lg shadow-xl" src={beachwork} alt="Woman workcationing on the beach" />
-        <h1 className="mt-6 text-2x1 font-bold text-gray-900">You can work from anywhere. <span className="text-indigo-500"> Take advantage of it. </span></h1>
-       <p className="mt-6 text-gray-600">Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some
+    <div className="bg-gray-300 ">
+
+      <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
+      <div>    
+        <div className="px-8 py-12 max-w-md mx-auto sm:max-w-x1 lg:px-12 lg:py-24 lg:max-w-full x1:mr-0 2xl:col-span-2">
+ <div className="x1:max-w-x1">
+
+        <img className="h-10" src={logobrand} alt="Workcation" />
+        <img className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+         src={beachwork} alt="Woman workcationing on the beach" />
+        <h1 className="mt-6 text-2x1 font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3x1 xl:text-4xl">You can work from anywhere.
+        <br className="hidden lg:inline" />
+         <span className="text-indigo-500"> Take advantage of it. </span></h1>
+       <p className="mt-6 text-gray-600 sm:mt-4 sm:text-x1">Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some
         nice weather even when you're not on vacation.</p>
-        <div className="mt-4">
-          <a className="inline-block rounded-lg px-5 py-3 shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm" href="">Book your escape</a>
+        <div className="mt-4 space-x-1 space-x-1 sm:mt-6">
+        <a className="btn btn-primary shadow-lg transform transition hover:-translate-y-0.5"href="#" >Book your escape</a>
+            <a  className="btn btn-secondary" href="#">Learn more</a>
+ </div>
         </div>
         </div>
       </div>
-      
-    
+      <div className="hidden relative lg:block 2xl:col-span-3">
+
+      <img  className="absolute inset-0 w-full h-full object-cover object-center" src={beachwork} alt="Woman workcationing on the beach" />
+      </div>
+        </div>
+
+      <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+        <h2 className="text-xl text-gray-900">Popular destinations</h2>
+        <p className="mt-2 text-gray-600">A selection of great work-friendly cities with lots to see and explore.</p>
+   
+        <div className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
+        {popularDestinations.map((destination) => (
+          <DestinationCard destination={destination} key={destination.city} />
+          ))}
+        </div>
+        </div>
   </div>
   );
 }
---
+++ src/data/popularDestinations.js
@@ -0,0 +1,53 @@
+import Toronto from '../img/toronto.jpg';
+import Malibu from '../img/malibu.jpg';
+import Miami from '../img/miami.jpg';
+import Seattle from '../img/seattle.jpg';
+import Colorado from '../img/colorado.jpg';
+import Chicago from '../img/chicago.jpg';
+
+const popularDestinations = [
+    {
+      city: "Toronto",
+      averagePrice: 120,
+      propertyCount: 76,
+      imageUrl: Toronto,
+      imageAlt: "Toronto skyline",
+    },
+    {
+      city: "Malibu",
+      averagePrice: 215,
+      propertyCount: 43,
+      imageUrl: Malibu,
+      imageAlt: "Cliff in Malibu",
+    },
+    {
+      city: "Chicago",
+      averagePrice: 130,
+      propertyCount: 115,
+      imageUrl: Chicago,
+      imageAlt: "Chicago skyline",
+    },
+    {
+      city: "Seattle",
+      averagePrice: 135,
+      propertyCount: 63,
+      imageUrl: Seattle,
+      imageAlt: "Seattle skyline",
+    },
+    {
+      city: "Colorado",
+      averagePrice: 85,
+      propertyCount: 47,
+      imageUrl: Colorado,
+      imageAlt: "Lake in Colorado",
+    },
+    {
+      city: "Miami",
+      averagePrice: 115,
+      propertyCount: 86,
+      imageUrl: Miami,
+      imageAlt: "Beach in Miami",
+    },
+  ];
+
+  export default popularDestinations;
